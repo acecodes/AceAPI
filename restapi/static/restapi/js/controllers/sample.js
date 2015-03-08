e312254968c4e7ee887450f6f0c8e6f597875403
Ace <ace.github@gmail.com>
2015-03-07 23:17:05 -0800

    More correct strict usage

--- restapi/static/restapi/js/controllers/sample.js
+++ restapi/static/restapi/js/controllers/sample.js
@@ -1,18 +1,19 @@
-(function() { 'use strict';
+(function() { 
+    'use strict';
 
-/**
- * @ngdoc function
- * @name frontendApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the frontendApp
- */
-angular.module('frontendApp')
-  .controller('SampleCtrl', function ($scope) {
-    $scope.testItems = [
-      'A is for Antelope',
-      'B is for Bison',
-      'C is for Cougar'
-    ];
-  });
+    /**
+     * @ngdoc function
+     * @name frontendApp.controller:AboutCtrl
+     * @description
+     * # AboutCtrl
+     * Controller of the frontendApp
+     */
+    angular.module('frontendApp')
+      .controller('SampleCtrl', function ($scope) {
+        $scope.testItems = [
+          'A is for Antelope',
+          'B is for Bison',
+          'C is for Cougar'
+        ];
+      });
 }());
